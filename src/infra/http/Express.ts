eaf99a67967afd3162ab5a28924055e1c964f35b
Renan S Pereira <renanspereira.dev@gmail.com>
2021-09-07 15:14:39 -0300

    add end point register todo

--- src/infra/http/Express.ts
+++ src/infra/http/Express.ts
@@ -2,11 +2,16 @@ import ControllerTodo from "../../controller/ControllerTodo";
 import Express from "express";
 
 const app = Express();
+app.use(Express.json()) 
 
-app.get('/todos', async function(req: any, res: any) {
+app.get("/todos", async function(req: any, res: any) {
   return res.json(await ControllerTodo.listTodos());
 });
 
-app.listen(3000, () => {
+app.post("/todos", function(req: any, res: any) {
+  return res.json(ControllerTodo.registerTodo(req.body.description, req.body.finished));
+});
+
+app.listen(3000, () => {''
     console.log('Server Online');
 });
