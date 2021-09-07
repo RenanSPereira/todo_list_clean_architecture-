044d26921d3a2c7210fe921bc9f2a1e3fbdffb5a
Renan S Pereira <renanspereira.dev@gmail.com>
2021-09-07 15:13:40 -0300

    add register method

--- src/controller/ControllerTodo.ts
+++ src/controller/ControllerTodo.ts
@@ -1,10 +1,18 @@
 import RepositoryMemory from "../infra/repository/RepositoryMemory";
 import ListTodos from "../core/useCases/ListTodos";
+import RegisterTodo from "../core/useCases/RegisterTodo";
+import { UUID } from "../lib/Generics";
 
 export default class ControllerTodo {
     static listTodos() {
         const repository = new RepositoryMemory();
         const listTodos = new ListTodos(repository);
         return listTodos.execute();
+    }
+
+    static registerTodo(description: string, finished: boolean) {
+        const repository = new RepositoryMemory();
+        const registerTodo = new RegisterTodo(repository);
+        return registerTodo.execute(UUID(), description, finished);
     }
 }
